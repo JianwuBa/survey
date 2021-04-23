758ab64bbd3f295330e545395249f7a93c8f0922
JianwuBai <641605665@qq.com>
2021-04-23 23:15:40 +0800

    echats

--- app.js
+++ app.js
@@ -22,12 +22,37 @@ app.post("/form",function (req,res) {
         })
      })
 })
-app.get("/object:id",function (req,res) {
-    console.log(req.params.id)
-    if(req.paramms.id == 1){
-        Survey.count({"object1":"0"},function (err,count_0) {
-            console.log(count_0)
+app.get("/object/:id",function (req,res) {
+    var result = req.params.id;
+    if(result == 1){
+        Survey.count({"object1":"0"},function(err,count_0){
+            Survey.count({"object1":"1"},function(err,count_1){
+                Survey.count({"object1":"2"},function(err,count_2){
+                    Survey.count({"object1":"3"},function(err,count_3){
+                        Survey.count({"object1":"4"},function(err,count_4){
+                            res.json({
+                                "success":true,
+                                "data":{"A":count_0,"B":count_1,"C":count_2,"D":count_3,"E":count_4}
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    }else if(result == 2){
+        Survey.count({"object2":"0"},function(err,count_0){
+            Survey.count({"object2":"1"},function(err,count_1){
+                Survey.count({"object2":"2"},function(err,count_2){
+                    Survey.count({"object2":"3"},function(err,count_3){
+                        res.json({
+                            "success":true,
+                            "data":{"A":count_0,"B":count_1,"C":count_2,"D":count_3}
+                        })
+                    })
+                })
+            })
         })
     }
 })
+app.get("/object")
 app.listen(3000)
